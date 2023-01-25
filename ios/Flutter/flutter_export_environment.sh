b0f2bc4280a93d527ef240af2c140f00d2c0905a
Joseph Cavanna <75166493+josephcavanna@users.noreply.github.com>
2023-01-25 14:10:50 +0100

    first commit

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/josephcavanna/Developer/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/josephcavanna/Development/Flutter Projects/babygrowth_2"
+export "FLUTTER_APPLICATION_PATH=/Users/josephcavanna/Development/Flutter Projects/babygrowth"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=/Users/josephcavanna/Development/Flutter Projects/babygrowth_2/lib/main.dart"
+export "FLUTTER_TARGET=/Users/josephcavanna/Development/Flutter Projects/babygrowth/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=2.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
@@ -11,4 +11,4 @@ export "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
-export "PACKAGE_CONFIG=/Users/josephcavanna/Development/Flutter Projects/babygrowth_2/.dart_tool/package_config.json"
+export "PACKAGE_CONFIG=/Users/josephcavanna/Development/Flutter Projects/babygrowth/.dart_tool/package_config.json"
