6e724d79886be6c5ceb53b966375ab9a90ca9342
Joseph Cavanna <75166493+josephcavanna@users.noreply.github.com>
2023-02-13 12:30:21 +0100

    minor updates

--- android/app/src/main/kotlin/com/example/babygrowth_2/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.jacavanna.babygrowthApp
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- android/app/src/main/kotlin/com/jacavanna/babygrowth/MainActivity.kt
+++ android/app/src/main/kotlin/com/jacavanna/babygrowth/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.jacavanna.babygrowthApp
+package com.jacavanna.babygrowth
 
 import io.flutter.embedding.android.FlutterActivity
 
